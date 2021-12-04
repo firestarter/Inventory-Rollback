8564d6791ef277a7837412b0354a9667934842cf
Mykyta Komarnytskyy <nkomarn@hotmail.com>
2021-12-04 00:54:25 -0800

    obf reflection for 1.18

--- src/main/java/me/danjono/inventoryrollback/reflections/NBT.java
+++ src/main/java/me/danjono/inventoryrollback/reflections/NBT.java
@@ -28,15 +28,15 @@ public class NBT {
 	public ItemStack setString(String key, String data) {        
 		try {
 			Object itemstack = packets.getCraftBukkitClass("inventory.CraftItemStack").getMethod("asNMSCopy", ItemStack.class).invoke(null, item);
-			Object comp = itemstack.getClass().getMethod("getTag").invoke(itemstack);
+			Object comp = itemstack.getClass().getMethod("s").invoke(itemstack);
 			
 			if (comp == null) {
 				comp = packets.getCraftBukkitClass("NBTTagCompound").newInstance();
 			}
 			
-			comp.getClass().getMethod("setString", String.class, String.class).invoke(comp, key, data);
+			comp.getClass().getMethod("a", String.class, String.class).invoke(comp, key, data);
 			
-			itemstack.getClass().getMethod("setTag", comp.getClass()).invoke(itemstack, comp);	
+			itemstack.getClass().getMethod("c", comp.getClass()).invoke(itemstack, comp);
 			item = (ItemStack) packets.getCraftBukkitClass("inventory.CraftItemStack").getMethod("asBukkitCopy", itemstack.getClass()).invoke(null, itemstack);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -48,15 +48,15 @@ public class NBT {
 	public ItemStack setInt(String key, int data) {
 		try {
 			Object itemstack = packets.getCraftBukkitClass("inventory.CraftItemStack").getMethod("asNMSCopy", ItemStack.class).invoke(null, item);
-			Object comp = itemstack.getClass().getMethod("getTag").invoke(itemstack);
+			Object comp = itemstack.getClass().getMethod("s").invoke(itemstack);
 			
 			if (comp == null) {
 				comp = packets.getCraftBukkitClass("NBTTagCompound").newInstance();
 			}
 			
-			comp.getClass().getMethod("setInt", String.class, int.class).invoke(comp, key, data);
+			comp.getClass().getMethod("a", String.class, int.class).invoke(comp, key, data);
 			
-			itemstack.getClass().getMethod("setTag", comp.getClass()).invoke(itemstack, comp);	
+			itemstack.getClass().getMethod("c", comp.getClass()).invoke(itemstack, comp);
 			item = (ItemStack) packets.getCraftBukkitClass("inventory.CraftItemStack").getMethod("asBukkitCopy", itemstack.getClass()).invoke(null, itemstack);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -68,15 +68,15 @@ public class NBT {
 	public ItemStack setLong(String key, Long data) {
 		try {
 			Object itemstack = packets.getCraftBukkitClass("inventory.CraftItemStack").getMethod("asNMSCopy", ItemStack.class).invoke(null, item);
-			Object comp = itemstack.getClass().getMethod("getTag").invoke(itemstack);
+			Object comp = itemstack.getClass().getMethod("s").invoke(itemstack);
 			
 			if (comp == null) {
 				comp = packets.getCraftBukkitClass("NBTTagCompound").newInstance();
 			}
 			
-			comp.getClass().getMethod("setLong", String.class, long.class).invoke(comp, key, data);
+			comp.getClass().getMethod("a", String.class, long.class).invoke(comp, key, data);
 			
-			itemstack.getClass().getMethod("setTag", comp.getClass()).invoke(itemstack, comp);	
+			itemstack.getClass().getMethod("c", comp.getClass()).invoke(itemstack, comp);
 			item = (ItemStack) packets.getCraftBukkitClass("inventory.CraftItemStack").getMethod("asBukkitCopy", itemstack.getClass()).invoke(null, itemstack);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -88,15 +88,15 @@ public class NBT {
 	public ItemStack setDouble(String key, Double data) {
 		try {
 			Object itemstack = packets.getCraftBukkitClass("inventory.CraftItemStack").getMethod("asNMSCopy", ItemStack.class).invoke(null, item);
-			Object comp = itemstack.getClass().getMethod("getTag").invoke(itemstack);
+			Object comp = itemstack.getClass().getMethod("s").invoke(itemstack);
 			
 			if (comp == null) {
 				comp = packets.getCraftBukkitClass("NBTTagCompound").newInstance();
 			}
 			
-			comp.getClass().getMethod("setDouble", String.class, double.class).invoke(comp, key, data);
+			comp.getClass().getMethod("a", String.class, double.class).invoke(comp, key, data);
 			
-			itemstack.getClass().getMethod("setTag", comp.getClass()).invoke(itemstack, comp);	
+			itemstack.getClass().getMethod("c", comp.getClass()).invoke(itemstack, comp);
 			item = (ItemStack) packets.getCraftBukkitClass("inventory.CraftItemStack").getMethod("asBukkitCopy", itemstack.getClass()).invoke(null, itemstack);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -108,15 +108,15 @@ public class NBT {
 	public ItemStack setFloat(String key, Float data) {
 		try {
 			Object itemstack = packets.getCraftBukkitClass("inventory.CraftItemStack").getMethod("asNMSCopy", ItemStack.class).invoke(null, item);
-			Object comp = itemstack.getClass().getMethod("getTag").invoke(itemstack);
+			Object comp = itemstack.getClass().getMethod("s").invoke(itemstack);
 			
 			if (comp == null) {
 				comp = packets.getCraftBukkitClass("NBTTagCompound").newInstance();
 			}
 			
-			comp.getClass().getMethod("setFloat", String.class, float.class).invoke(comp, key, data);
+			comp.getClass().getMethod("a", String.class, float.class).invoke(comp, key, data);
 			
-			itemstack.getClass().getMethod("setTag", comp.getClass()).invoke(itemstack, comp);	
+			itemstack.getClass().getMethod("c", comp.getClass()).invoke(itemstack, comp);
 			item = (ItemStack) packets.getCraftBukkitClass("inventory.CraftItemStack").getMethod("asBukkitCopy", itemstack.getClass()).invoke(null, itemstack);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -131,9 +131,9 @@ public class NBT {
 		
 		try {
 			Object itemstack = packets.getCraftBukkitClass("inventory.CraftItemStack").getMethod("asNMSCopy", ItemStack.class).invoke(null, item);
-			try { comp = itemstack.getClass().getMethod("getTag").invoke(itemstack); } catch (NullPointerException e) { return null; }
+			try { comp = itemstack.getClass().getMethod("s").invoke(itemstack); } catch (NullPointerException e) { return null; }
 			
-			try { result = comp.getClass().getMethod("getString", String.class).invoke(comp, key); } catch (NullPointerException e) { return null; }
+			try { result = comp.getClass().getMethod("l", String.class).invoke(comp, key); } catch (NullPointerException e) { return null; }
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -146,9 +146,9 @@ public class NBT {
 		
 		try {
 			Object itemstack = packets.getCraftBukkitClass("inventory.CraftItemStack").getMethod("asNMSCopy", ItemStack.class).invoke(null, item);
-			Object comp = itemstack.getClass().getMethod("getTag").invoke(itemstack);
+			Object comp = itemstack.getClass().getMethod("s").invoke(itemstack);
 			
-			try { result = comp.getClass().getMethod("getInt", String.class).invoke(comp, key); } catch (NullPointerException e) {}
+			try { result = comp.getClass().getMethod("h", String.class).invoke(comp, key); } catch (NullPointerException e) {}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -161,9 +161,9 @@ public class NBT {
 		
 		try {
 			Object itemstack = packets.getCraftBukkitClass("inventory.CraftItemStack").getMethod("asNMSCopy", ItemStack.class).invoke(null, item);
-			Object comp = itemstack.getClass().getMethod("getTag").invoke(itemstack);
+			Object comp = itemstack.getClass().getMethod("s").invoke(itemstack);
 			
-			try { result = comp.getClass().getMethod("getLong", String.class).invoke(comp, key); } catch (NullPointerException e) {}
+			try { result = comp.getClass().getMethod("i", String.class).invoke(comp, key); } catch (NullPointerException e) {}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -176,9 +176,9 @@ public class NBT {
 		
 		try {
 			Object itemstack = packets.getCraftBukkitClass("inventory.CraftItemStack").getMethod("asNMSCopy", ItemStack.class).invoke(null, item);
-			Object comp = itemstack.getClass().getMethod("getTag").invoke(itemstack);
+			Object comp = itemstack.getClass().getMethod("s").invoke(itemstack);
 			
-			try { result = comp.getClass().getMethod("getDouble", String.class).invoke(comp, key); } catch (NullPointerException e) {}
+			try { result = comp.getClass().getMethod("k", String.class).invoke(comp, key); } catch (NullPointerException e) {}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -191,9 +191,9 @@ public class NBT {
 		
 		try {
 			Object itemstack = packets.getCraftBukkitClass("inventory.CraftItemStack").getMethod("asNMSCopy", ItemStack.class).invoke(null, item);
-			Object comp = itemstack.getClass().getMethod("getTag").invoke(itemstack);
+			Object comp = itemstack.getClass().getMethod("s").invoke(itemstack);
 			
-			try { result = comp.getClass().getMethod("getFloat", String.class).invoke(comp, key); } catch (NullPointerException e) {}
+			try { result = comp.getClass().getMethod("j", String.class).invoke(comp, key); } catch (NullPointerException e) {}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
